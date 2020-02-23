cbf199dd5a3dd461d7a8a3fb884799bb5ee0f649
rainbow <info@rainbow23.jp>
2020-02-24 01:15:35 +0900

    first commit

---
+++ mysql/README.md
@@ -0,0 +1,2 @@
+docker-compose up --build -d  
+mysql -h 127.0.0.1  --port=43306 -u docker -pdocker
---
+++ mysql/init/01_initial_db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS todo;
+CREATE TABLE IF NOT EXISTS todo.tasks(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(100),
+  body VARCHAR(1000),
+  created_at TIMESTAMP NOT NULL default current_timestamp,
+  updated_at TIMESTAMP NOT NULL default current_timestamp on update current_timestamp
+);
+
+GRANT ALL ON todo.* TO docker@'%' IDENTIFIED BY 'docker';
